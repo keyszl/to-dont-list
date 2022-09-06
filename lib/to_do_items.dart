0829ac16122da4eead97f06c145dff4f11fc0495
olivia <larsonog@hendrix.edu>
2022-09-06 14:53:50 -0500

    Circle Avatar test complete

--- lib/to_do_items.dart
+++ lib/to_do_items.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 class Item {
   const Item({required this.name});
 
@@ -33,7 +34,7 @@ class ToDoListItem extends StatelessWidget {
     // taking place and therefore which theme to use.
 
     return completed //
-        ? Colors.black
+        ? Colors.black54
         : Theme.of(context).primaryColor;
   }
 
@@ -59,10 +60,10 @@ class ToDoListItem extends StatelessWidget {
           : null,
       leading: CircleAvatar(
         backgroundColor: _getColor(context),
-        child: Text(item.name),
+        child: Text(item.abbrev()),
       ),
       title: Text(
-        item.abbrev(),
+        item.name,
         style: _getTextStyle(context),
       ),
     );
